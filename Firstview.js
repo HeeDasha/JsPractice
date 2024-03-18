6e43c1706d04316cdf344128e85ebbecc2714f55
김희찬 <kimheechan13@gmail.com>
2024-03-18 22:13:10 +0900

    그냥 아무거나 작성하깅

--- Firstview.js
+++ Firstview.js
@@ -1,7 +1,24 @@
-const Account = {
- 
-    name : '김희찬',
-    play : "programmer",
-    roleplay : "개발자",
+//전역변수 선언 
+const name = "김희찬";
+
+
+
+//함수를 선언하여 재활용 기능 활성화 
+
+function AccountRe(age,job){
+
+
+    
+    let age = prompt("나이를 입력해주세요", );
+    let job = prompt("직업을 입력해주세요", );
+
+    console.log( `${name}님은 나이가 ${age}살이고 직업은 ${job} 입니다.`);
+
 
 }
+
+
+
+AccountRe();
+
+
