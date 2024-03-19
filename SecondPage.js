955faf5d7cbe39a7c8cec8d01d332b174628f650
Hee chan <kimheechan13@gmail.com>
2024-03-19 14:16:59 +0900

    기존의 변경

---
+++ SecondPage.js
@@ -0,0 +1,52 @@
+// const Peter = {
+
+//     name : '김희찬',
+//     age : 30,
+
+// };
+
+// Peter.job = '프로그래머';
+
+// console.log(Peter);
+// console.log(typeof Peter);
+
+
+//객지 지정 키워드를 const로 하면 객체에 값을 추가하면 const의 규칙에 위배가 되는게 아닌가 값이 변하면 안될텐데...이게 왜 될까.
+
+
+// const A = prompt("나이를 입력해주세요",);
+
+// console.log(A);
+
+// const a = 1;
+// const b = "1";
+
+// console.log(a===b);
+
+//성인이고 혈액형이 O형이고 남자인 사람을 판별하는 코드를 짜보시오.
+
+const Name ="김희찬"
+
+
+function FindAdult() {
+
+
+  let Adult = prompt("나이를 입럭하세요",);
+  let BloodType = prompt("혈액형을 입력하세요",);
+  let Gender = prompt("성별을 입력하세요",);
+
+  if(Adult >= 20 && BloodType == "O" && Gender == "남자"){
+    alert(`환영합니다 ${Name}님`);
+
+  }else{
+    alert(`당신은 ${Name}님이 아닙니다.`);
+  }
+
+}
+
+FindAdult();
+
+
+
+
+
