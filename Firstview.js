a0f09ad9f9c5d9915265df113d2f1e7fa486bdf1
김희찬 <kimheechan13@gmail.com>
2024-03-20 21:21:47 +0900

    commit

--- Firstview.js
+++ Firstview.js
@@ -12,14 +12,14 @@ function AccountRe(){
     let age = prompt("나이를 입력해주세요", ); // 나이 값 할당
     let job = prompt("직업을 입력해주세요", ); // 직업 값 할당
 
-    console.log( `${name}님은 나이가 ${age}살이고 직업은 ${job} 입니다.`); //백틱으로 값 확인
+    alert( `${name}님은 나이가 ${age}살이고 직업은 ${job} 입니다.`); //백틱으로 값 확인
 
 
 }
 
 
 
-AccountRe(age,job); // 함수 호출 
+AccountRe(); // 함수 호출 
 
 
 
