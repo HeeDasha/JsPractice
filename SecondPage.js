c59410507da064db5ef6e9d9511a5048be47e454
Kim Hee Chan <kimheechan13@gmail.com>
2024-03-21 13:58:54 +0900

    commit

--- SecondPage.js
+++ SecondPage.js
@@ -11,7 +11,7 @@
 // console.log(typeof Peter);
 
 
-//객지 지정 키워드를 const로 하면 객체에 값을 추가하면 const의 규칙에 위배가 되는게 아닌가 값이 변하면 안될텐데...이게 왜 될까.
+// 객체 지정 키워드를 const로 하면 객체에 값을 추가하면 const의 규칙에 위배가 되는게 아닌가 값이 변하면 안될텐데...이게 왜 될까.'
 
 
 // const A = prompt("나이를 입력해주세요",);
@@ -25,26 +25,27 @@
 
 //성인이고 혈액형이 O형이고 남자인 사람을 판별하는 코드를 짜보시오.
 
-const Name ="김희찬"
+// const Name ="김희찬"  // 전역변수 선언
 
 
-function FindAdult() {
+// function FindAdult() {
 
 
-  let Adult = prompt("나이를 입럭하세요",);
-  let BloodType = prompt("혈액형을 입력하세요",);
-  let Gender = prompt("성별을 입력하세요",);
+//   let Adult = prompt("나이를 입럭하세요",);
+//   let BloodType = prompt("혈액형을 입력하세요",);
+//   let Gender = prompt("성별을 입력하세요",);
 
-  if(Adult >= 20 && BloodType == "O" && Gender == "남자"){
-    alert(`환영합니다 ${Name}님`);
+//   if(Adult >= 20 && BloodType == "O" && Gender == "남자"){
+//     alert(`환영합니다 ${Name}님`);
 
-  }else{
-    alert(`당신은 ${Name}님이 아닙니다.`);
-  }
+//   }else{
+//     alert(`당신은 ${Name}님이 아닙니다.`);
+//   }
 
-}
+// }
 
-FindAdult();
+// FindAdult();  // 함수 호출 
+
 
 
 
