da7c23651bb67b4214982af84d0e8a56323c1405
Kim Hee Chan <kimheechan13@gmail.com>
2024-03-21 19:13:52 +0900

    commit

--- 개인 공부/SecondPage.js
+++ 개인 공부/SecondPage.js
@@ -23,7 +23,7 @@
 
 // console.log(a===b);
 
-//성인이고 혈액형이 O형이고 남자인 사람을 판별하는 코드를 짜보시오.
+/* 성인이고 혈액형이 O형이고 남자인 사람을 판별하는 코드를 짜보시오. */
 
 // const Name ="김희찬"  // 전역변수 선언
 
@@ -46,8 +46,89 @@
 
 // FindAdult();  // 함수 호출 
 
+//2024-03-21 반복문 학습
+
+
+/* for 문을 활용하여 1부터 10까지 콘솔로 찍어보자 */
+
+// for(let i=0; i<11; i++) {
+//     console.log(i);
+// }
+
+/* while 문을 활용하여 1부터 10까지 콘솔로 찍어보자 */
+
+//  let i = 0;  //초기 전역변수 선언
+
+// while(i<11) {
+
+//     console.log(i)
+//     i++
+
+// }
+
+
+/* Do while 문을 활용하여 1부터 10까지 콘솔로 찍어보자 */
+
+// do {
+
+//     console.log(i);
+//     i++;
+
+// } while(i<10)
+
+/* Do while은 첫번째는 조건과 상관없이 무조건 실행된다 */
 
 
 
+/* Break 활용 */
 
+// while(true){
+
+//     let answer = confirm('진행할껴?');
+//     if(!answer) {
+//         break;
+//     }
+// }
+
+/* While문을 통화 짝수를 구하며 Continue 활용 */
+
+// let i = 0;
+
+// while(i<10){
+
+//     i++;
+//     if(i%2){
+//         continue;
+
+//     }
+
+//     console.log(i);
+// }
+
+/* Do white 문 활용하여 짝수 출력 */
+
+// let i = 0;
+
+// do {
+//     i++;
+//     if(i%2) {
+//         continue;
+//     }
+//     console.log(i)
+
+// }while(i<10)
+
+/* For 문을 활용하여 짝수 출력 */
+
+for(let i = 0; i<10 ; i++) {
+    
+    if(i%2){
+
+        continue;
+    }
+    console.log(i);
+}
+
+/* 알 수 있는건 For문은 0이 출력이 되고 While과 Do while은 0이 출력되지 않는다. 이유는 while 과 Do while은 i++ 를 진행을 먼저 하기에 0+1= 1, 즉 1부터 시작하기 때문이다. */
+/* 반면에 For문은 반복을 진행 후 증감이 되기 때문에 0부터 출력이 된다. */
 
