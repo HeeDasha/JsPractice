b1afae744851cfa2d09c3ef46d634e84b08b30c9
Kim Hee Chan <kimheechan13@gmail.com>
2024-03-25 16:13:27 +0900

    commit

--- 개인 공부/SecondJsLessonCycle.js
+++ 개인 공부/SecondJsLessonCycle.js
@@ -28,7 +28,7 @@
 // const Name ="김희찬"  // 전역변수 선언
 
 
-// function FindAdult() {
+// let FindAdult = function() {
 
 
 //   let Adult = prompt("나이를 입럭하세요",);
@@ -134,25 +134,67 @@
 
 /* 사고 싶은 차를 물어보고 가격을 알려주는 Switch 문 */
 
-let car = prompt('무슨 차를 사고 싶나요?');
+// let car = prompt('무슨 차를 사고 싶나요?');
 
-switch(car) {
-    case '투싼' :
-        alert('200만원입니다.');
-        break;
-    case '모닝' :
-        alert('500만원');
-        break;
-    case '제네시스' :
-        alert('5000만원');
-        break ;
-    case '싼타페' :
-        alert('1000만원');
-        break ;
-    default :
-        alert('그런 자동차는 없습니다');   
+// switch(car) {
+//     case '투싼' :
+//         alert('200만원입니다.');
+//         break;
+//     case '모닝' :
+//         alert('500만원');
+//         break;
+//     case '제네시스' :
+//         alert('5000만원');
+//         break ;
+//     case '싼타페' :
+//         alert('1000만원');
+//         break ;
+//     default :
+//         alert('그런 자동차는 없습니다');   
 
-}
+// }
+
+/* 함수 표현식 && 화살표 함수  */
+
+/* 기본 함수 */
+
+// let somefunction = function(num1,num2) {
+
+  
+//     const sum = Number(num1) + Number(num2)
+//     alert(sum);
+   
+// }
+
+// let result = prompt('숫자를 입력하세요',);
+// let result2 = prompt(`다음 숫자를 입력하세요`);
+
+// somefunction(result,result2);
+
+/* 화살표 함수 */
+
+// let arrowfunction = (num1,num2) => {
+    
+//     const sum = Number(num1) + Number(num2);
+//     alert(sum);
+
+// }
+
+// let result3 = prompt('숫자를 입력해주세요', );
+// let result4 = prompt('다음 숫자를 입력해주세요', );
+
+// arrowfunction(result3,result4);
+
+
+
+// 화살표 함수는 코드 본문이 한 줄이고 리턴문이 있다면 중괄호 대신 일반괄호로 바꿀 수 있다.
+// 리턴문이 한 줄 일 경우 괄호 또한 생략 가능.
+// 인수가 하나라면 인수의 괄호 또한 생략 가능.
+// 인수가 없는 함수는 인수의 괄호를 생략 할 수 없다.
+// 리턴 전에 여러 줄의 코드가 있을 경우 일반괄호를 사용할 수 없다.
+
+
+
 
 
 
