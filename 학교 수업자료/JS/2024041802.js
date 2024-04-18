87612267d933a27be977fdb2b6e1971481514067
Kim Hee Chan <kimheechan13@gmail.com>
2024-04-18 18:18:43 +0900

    2024-04-18

--- 학교 수업자료/JS/2024041802.js
+++ 학교 수업자료/JS/2024041802.js
@@ -1,6 +1,6 @@
 function over(obj){
-    obj.src = "/학교 수업자료/Image/apple.jpg";
+    obj.src = "/JsPractice/학교 수업자료/Image/apple.jpg";
 }
 function out(obj){
-    obj.src="/학교 수업자료/Image/bannana.jpg";
+    obj.src="/JsPractice/학교 수업자료/Image/bannana.jpg";
 }
